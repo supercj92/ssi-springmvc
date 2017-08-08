b4ebb1229339ddf06a3f7df91a026af4a6238069
supercj92 <ukd163@163.com>
2017-08-08 21:48:05 +0800

    create table sql

---
+++ src/main/resources/creatTable.sql
@@ -0,0 +1,22 @@
+CREATE TABLE `order` (
+  `id` int(11) NOT NULL auto_increment,
+  `customer_name` varchar(45) NOT NULL,
+  `skuid` int(11) NOT NULL,
+  `num` int(11) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
+
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL auto_increment,
+  `product_name` varchar(45) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL auto_increment,
+  `user_name` varchar(45) default NULL,
+  `pwd` varchar(45) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
