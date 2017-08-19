c94caf7bc7f40b4df0b82e7b6564fac40bcaef9a
supercj92 <ukd163@163.com>
2017-08-19 21:41:49 +0800

    学习uploadify

--- src/main/java/com/cfysu/ssi/controller/FrontPageController.java
+++ src/main/java/com/cfysu/ssi/controller/FrontPageController.java
@@ -1,8 +1,8 @@
 package com.cfysu.ssi.controller;
 
-import com.alibaba.fastjson.JSON;
 import com.cfysu.ssi.model.User;
 import com.cfysu.ssi.service.UserService;
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -11,8 +11,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 import java.util.Date;
 
 @Controller
@@ -42,8 +48,33 @@ public class FrontPageController {
 		user.setUserName("rose");
 		user.setPwd("456");
 		userService.insert(user);
-
 		return "ok";
+	}
+
+	/**
+	 * 上传文件
+	 * @return
+	 */
+	@RequestMapping("/toUpload")
+	public String toUpload(){
+		return "upload";
+	}
+
+	@RequestMapping("/upload")
+	@ResponseBody
+	public String uploadFile(MultipartFile file){
+		if(file == null){
+			return "文件为空";
+		}
+		LOGGER.info("fileName:{}", file.getOriginalFilename());
+		URL url = FrontPageController.class.getClassLoader().getResource("/");
+		try {
+			file.transferTo(new File(url.getPath() + File.separator + file.getOriginalFilename()));
+		} catch (IOException e) {
+			e.printStackTrace();
+			return "fail";
+		}
+		return "success";
 	}
 
 }
