7e76d6e29ece231419553512cc022ec48f4f6512
cj <ukd163@163.com>
2017-08-19 15:28:50 +0800

    学习uploadify的使用

--- src/main/webapp/asset/js/myFileUpload.js
+++ src/main/webapp/asset/js/myFileUpload.js
@@ -1,7 +1,8 @@
 $(function () {
     $('#file-upload').uploadify({
-        'swf' : 'lib/uploadify/uploadify.swf',
-        'uploader' : '/upload'
+        'swf' : '/asset/js/lib/uploadify/uploadify.swf',
+        'uploader' : '/upload',
+        'buttonText' : '上传文件'
     });
     $('#beginUpload').click(function () {
         $('#file-upload').uploadify('upload');
