73d234ebe2751431fd82ce8832cf0bb3f580a8bd
cj <ukd163@163.com>
2017-08-20 11:07:42 +0800

    基于注解配置定时器

--- src/main/java/com/cfysu/ssi/worker/impl/SpringTimerImpl.java
+++ src/main/java/com/cfysu/ssi/worker/impl/SpringTimerImpl.java
@@ -2,6 +2,7 @@ package com.cfysu.ssi.worker.impl;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import com.cfysu.ssi.worker.SpringTimer;
@@ -10,6 +11,7 @@ import com.cfysu.ssi.worker.SpringTimer;
 public class SpringTimerImpl implements SpringTimer {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(SpringTimerImpl.class);
+	@Scheduled(cron ="*/10 * * * * ?")
 	public void print() {
 		LOGGER.info("-------------timer is running--------------");
 	}
