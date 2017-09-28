80cb6e29f0158093bf04e72d8341a32af5b93a89
supercj92 <ukd163@163.com>
2017-09-28 20:16:02 +0800

    hessian spring集成、autocomplete、toastr、my97-datepicker、ajaxFileUpload、jquery-validation

--- src/main/java/com/cfysu/ssi/jms/info.md
+++ src/main/java/com/cfysu/ssi/jms/info.md
@@ -1,2 +1,5 @@
 运行前提：启动activemq服务
-======================
+=
+/bin/activemq start
+
+http://127.0.0.1:8161/admin/
--- src/main/java/com/cfysu/ssi/rpc/HessianServerServiceImpl.java
+++ src/main/java/com/cfysu/ssi/rpc/HessianServerServiceImpl.java
@@ -1,10 +1,12 @@
 package com.cfysu.ssi.rpc;
 
 import com.test.hessian.service.HelloService;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by cj on 2017/9/1.
  */
+@Service
 public class HessianServerServiceImpl implements HelloService{
 
     public String sayHello(String str) {
